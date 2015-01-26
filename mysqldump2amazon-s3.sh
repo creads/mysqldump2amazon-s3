34e99d66d100891291b47e7a92a1d7f728fa63b1
Quentin <q.pautrat@creads.org>
2015-01-26 14:11:41 +0100

    create script

---
+++ mysqldump2amazon-s3.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# BEGIN EDIT
+
+DB_USER="root"
+DB_PASSWORD="password"
+DB_NAME=--all-databse
+TMP_PATH=~/
+BACKUP_NAME=backup
+S3_BUCKET=bucket
+S3_PATH=folder
+
+# END EDIT
+
+BACKUP_FILE=${TMP_PATH}${BACKUP_NAME}$(date +"_%d-%m-%Y_%H:%M:%S").sql.gz
+
+echo "Dump database ${DB_NAME}"
+
+mysqldump --user=${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} | gzip > ${BACKUP_FILE}
+
+echo "Backup file ${BACKUP_FILE} created"
+
+echo "Put file into Amazon S3 in s3://${S3_BUCKET}/${S3_PATH}/"
+
+s3cmd put -f ${BACKUP_FILE} s3://${S3_BUCKET}/${S3_PATH}/
+
+echo "Remove ${BACKUP_FILE}"
+
+rm -f ${BACKUP_FILE}
+
+echo "Backup removed"
